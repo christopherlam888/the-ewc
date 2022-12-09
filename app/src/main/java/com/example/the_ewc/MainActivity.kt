ecf7cd647fa7947285972aa4e6e52038c3ec6d63
verdantbreeze <chwil@Borealis.localdomain>
2022-12-09 15:42:13 -0500

    theme changes + main page ui change (broken textview)

--- app/src/main/java/com/example/the_ewc/MainActivity.kt
+++ app/src/main/java/com/example/the_ewc/MainActivity.kt
@@ -4,14 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val glossaryButton = findViewById<Button>(R.id.glossary_button)
-        val videosButton = findViewById<Button>(R.id.videos_button)
+        val glossaryButton = findViewById<ImageButton>(R.id.glossary_button)
+        val videosButton = findViewById<ImageButton>(R.id.videos_button)
         glossaryButton.setOnClickListener{
             val intent = Intent(this, GlossaryActivity::class.java)
             startActivity(intent)
